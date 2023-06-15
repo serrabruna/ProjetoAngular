c983765a1992665ee6bdbbbd237c77c39ec7671f
serrabruna <125033654+serrabruna@users.noreply.github.com>
2023-06-15 12:04:56 +0000

    desenvolvimento do layout

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,12 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
-  constructor() {}
+
+	exibir(numero: number) {
+		console.log(numero);
+	}
+
 }
